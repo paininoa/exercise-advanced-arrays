02b6d74331a66d2dcae8dbadba467c8f42c98b43
Alessio <carninoalessio@gmail.com>
2023-11-17 16:03:10 +0100

    Esercizio 1-2 17/11

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+//--------------------------------------------------------
+//-----------ESERCIZI 16/11 ----------(sotto ci sono quelli del 17/11)
+//--------------------------------------------------------
+
 /*
 1. Crea una funzione safeParseJSON che prende una stringa e tenta di analizzarla come JSON. 
 Se l'analisi fallisce, la funzione dovrebbe restituire null invece di generare un errore. 
@@ -113,6 +117,7 @@ una lunghezza minima. Dovrebbe restituire un nuovo array contenente solo
 le stringhe che sono più lunghe della lunghezza minima. Usa .filter().
 */
 
+/*
 const removeShortWords = (array, minLength) => {
   const filteredArray = array.filter((stringa) => stringa.length > minLength);
   return filteredArray;
@@ -121,3 +126,125 @@ const removeShortWords = (array, minLength) => {
 const testArray = ["verde", "giallo", "seg", "gesargheq"];
 const risultato = removeShortWords(testArray, 4);
 console.log(risultato);
+*/
+
+//--------------------------------------------------------
+//-----------ESERCIZI 17/11 ------------------------------
+//--------------------------------------------------------
+
+/*
+1. Ordina e Visualizza Film
+
+   JSON di partenza: `[
+    { id: 1, title: "Inception", year: 2010 }, 
+   { id: 2, title: "The Matrix", year: 1999 }, 
+   { id: 3, title: "Interstellar", year: 2014 }
+  ]`
+
+   - Esercizio: Crea una funzione `displaySortedFilms` che prende un array 
+   di film e li ordina per anno di uscita in ordine crescente. La funzione 
+   dovrebbe poi creare e aggiungere un elenco di elementi `<li>` al DOM, 
+   ognuno contenente il titolo del film e l'anno di uscita.
+*/
+
+/*
+const movies = [
+  { id: 1, title: "Inception", year: 2010 },
+  { id: 2, title: "The Matrix", year: 1999 },
+  { id: 3, title: "Interstellar", year: 2014 },
+  { id: 4, title: "Thrix", year: 1979 },
+];
+
+window.addEventListener("load", () => {
+  const displaySortedFilms = movies.sort((a, b) => {
+    return a.year - b.year;
+  });
+
+  const body = document.body;
+  const ul = document.createElement("ul");
+  body.appendChild(ul);
+  movies.forEach((movie) => {
+    const li = document.createElement("li");
+    li.innerText = `${movie.title}, ${movie.year}`;
+    ul.appendChild(li);
+  });
+});
+*/
+
+/*
+2. Crea una Lista di Libri Raccomandati
+   - JSON di partenza: `[{ id: 1, title: "1984", author: "George Orwell", rating: 4.5 }, { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien", rating: 4.7 }, { id: 3, title: "Brave New World", author: "Aldous Huxley", rating: 4.2 }]`
+   - Esercizio: Scrivi una funzione `listRecommendedBooks` che prende 
+   un array di libri e un punteggio minimo. La funzione dovrebbe filtrare 
+   i libri con un rating superiore o uguale al punteggio minimo, ordinarli 
+   per rating in ordine decrescente e creare una lista ordinata `<ol>` nel 
+   DOM con i titoli e gli autori dei libri raccomandati.
+*/
+
+/*
+const libri = [
+  { id: 1, title: "1984", author: "George Orwell", rating: 4.5 },
+  { id: 2, title: "The Hobbit", author: "J.R.R. Tolkien", rating: 4.7 },
+  { id: 3, title: "Brave New World", author: "Aldous Huxley", rating: 4.2 },
+  { id: 4, title: "Brave New World", author: "Aldous Huxley", rating: 5.2 },
+];
+
+window.addEventListener("load", () => {
+  const listRecommendedBooks = (listaLibri, punteggioMin) => {
+    const filtro = listaLibri.filter((numero) => numero.rating >= punteggioMin);
+
+    const ordina = filtro.sort((a, b) => {
+      if (a.rating > b.rating) {
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+
+    const body = document.body;
+    const ol = document.createElement("ol");
+    body.appendChild(ol);
+    ordina.forEach((libro) => {
+      const li = document.createElement("li");
+      li.innerText = `${libro.title}, ${libro.author}`;
+      ol.appendChild(li);
+    });
+  };
+
+  listRecommendedBooks(libri, 4.5);
+});
+*/
+
+/*
+3. Elenco Utenti Ordinati per Età
+   - JSON di partenza: `[{ id: 1, name: "Alice", age: 30 }, { id: 2, name: "Bob", age: 24 }, { id: 3, name: "Charlie", age: 28 }]`
+   
+   - Esercizio: Implementa una funzione `displayUsersByAge`
+    che prende un array di utenti e li ordina per età in ordine crescente. 
+    La funzione dovrebbe poi creare una tabella nel DOM con le righe che 
+    rappresentano gli utenti, mostrando il nome e l'età.
+*/
+
+const persone = [
+  { id: 1, name: "Alice", age: 30 },
+  { id: 2, name: "Bob", age: 24 },
+  { id: 3, name: "Charlie", age: 28 },
+];
+
+/*
+   4. Filtro e Ordinamento di Prodotti
+   - JSON di partenza: `[{ id: 1, name: "Laptop", price: 999.99 }, { id: 2, name: "Smartphone", price: 599.99 }, { id: 3, name: "Tablet", price: 399.99 }]`
+   - Esercizio: Crea una funzione `filterAndSortProducts` che prende un array di prodotti e un prezzo massimo. La funzione dovrebbe filtrare i prodotti che hanno un prezzo inferiore o uguale al prezzo massimo, ordinarli per prezzo in ordine crescente e visualizzare i risultati in una lista `<ul>` nel DOM, mostrando il nome e il prezzo di ciascun prodotto.
+*/
+
+/*
+5. Genera un Elenco di Punti di Interesse
+   - JSON di partenza: `[{ id: 1, name: "Eiffel Tower", location: "Paris", visited: true }, { id: 2, name: "Colosseum", location: "Rome", visited: false }, { id: 3, name: "Statue of Liberty", location: "New York", visited: true }]`
+   - Esercizio: Scrivi una funzione `createPOIList` che prende un array di punti di interesse (POI) e genera un elenco nel DOM. I POI dovrebbero essere ordinati alfabeticamente per nome e ogni elemento dell'elenco dovrebbe includere il nome del POI e la sua posizione. Aggiungi una classe CSS `visited` agli elementi che rappresentano i POI già visitati.
+*/
+
+/*
+   6. Classifica e Mostra Risultati di Gare
+   - JSON di partenza: `[{ id: 1, name: "John Doe", time: "00:23:15" }, { id: 2, name: "Jane Smith", time: "00:22:05" }, { id: 3, name: "Emily Davis", time: "00:25:30" }]`
+   - Esercizio: Implementa una funzione `displayRaceResults` che prende un array di risultati di gare e li ordina in base al tempo di completamento (dal più veloce al più lento). La funzione dovrebbe poi creare una lista `<ul>` nel DOM, dove ogni elemento `<li>` mostra il nome del partecipante e il suo tempo di gara.
+*/
